13fa7fe175d8b8c876ae7942f1503eda3e384cca
SossenSystems <33610430+SossenSystems@users.noreply.github.com>
2018-04-25 02:43:36 +0200

    Update ListWeblistUsers.php
    The code was revised

--- ListWeblistUsers.php
+++ ListWeblistUsers.php
@@ -1,81 +1,122 @@
 <?php
 include('ts3phpframework-1.1.32/libraries/TeamSpeak3/TeamSpeak3.php');
-$ts3_VirtualServer = TeamSpeak3::factory("serverquery://serveradmin:2399sdjsj9fj932@127.0.0.1:10011/?server_port=9987&nickname=[12-ATTENTION]%20TS3Public.de");	
 
-class ConsoleQuestion{
-    function readline(){
-        return rtrim(fgets(STDIN));
-    }
-}
+/* TS3 Query login */
+$ts3login['username'] = "serveradmin";
+$ts3login['password'] = "password";
+$ts3login['address'] = "127.0.0.1";
+$ts3login['queryport'] = '1976';
+$ts3login['voiceport'] = '9987';
+$ts3login['nickname'] = "User-Tracker";
 
-$line = new ConsoleQuestion();
-	
+/* Header for command line */
+$header[1] = "╔══════════════════════════════════════════════════";
+$header[2] = "║ ╔════════════════════════════════════════════════════╗";
+$header[3] = "║ ║                    Declarations                    ║";
+$header[4] = "║ ║    Real -> Real connection User's without Proxy    ║";
+$header[5] = "║ ║          German -> German Weblist User's           ║";
+$header[6] = "║ ║      Intern. -> International Weblist-User's       ║";
+$header[7] = "║ ╚════════════════════════════════════════════════════╝";
+$header[8] = "╚══════════════════════════════════════════════════";
+
+/* IP list for checking in Array */
+$ipListToCheck = array( "185.250.251.133", "159.100.21.154", "185.250.251.141" );
+
+/* Color set */
+$color['green'] = "\033[32m";
+$color['cyan'] = "\033[36m";
+$color['yellow'] = "\033[33m";
+$color['purple'] = "\033[35";
+$color['red'] = "\033[31m";
+$color['grey'] = "\033[1;32m";
+$color['lightblue'] = "\033[1;34m";
+$color['lightcyan'] = "\033[1;36m";
+
+/* Other */
 $newline = "\r\n";
-
+$set['first'] = "║ >";
+$set['end'] = "\033[0m";
 $e = 0;
 $ii = 0;
-$sleepcount = 300;
-while (true) {
-	try{
-		echo "╔══════════════════════════════════════════════════\n";
-		echo "║ Declarations list\n";
-		echo "║ German Weblist User's ●\n";
-		echo "║ International Weblist-User's ○\n";
-		echo "║ Real IP User's\n";
-		echo "║ -----------------\n";
+$footerLine = "════════════════════════════";
+
+$ts3_VirtualServer = TeamSpeak3::factory("serverquery://" . $ts3login['username'] . ":" . $ts3login['password'] . "@" . $ts3login['address'] . ":" . $ts3login['queryport'] . "/?server_port=" . $ts3login['voiceport'] . "&nickname=" . $ts3login['nickname']);	
+
+function runChecker() {
+	global $ts3_VirtualServer;
+	global $header;
+	global $ipListToCheck;
+	global $color;
+	global $newline;
+	global $set;
+	global $e;
+	global $ii;
+	try {
+		echo $header[1] . $newline;
+		echo $header[2] . $newline;
+		echo $header[3] . $newline;
+		echo $header[4] . $newline;
+		echo $header[5] . $newline;
+		echo $header[6] . $newline;
+		echo $header[7] . $newline;
 		foreach ($ts3_VirtualServer->clientList() as $client) {
 			if($client->client_type == "0"){
-				if($client['connection_client_ip'] == "185.250.251.133" OR $client['connection_client_ip'] == "159.100.21.154" OR $client['connection_client_ip'] == "185.250.251.141"){
-					try{
+				if(in_array($client['connection_client_ip'], $ipListToCheck)) {
+					try {
 						if($client['client_country'] == "DE"){
 							$e++;
-							echo "║ > \033[32m ● " . $client['client_nickname'] . " " . $e . "\033[0m\n";
+							echo $set['first'] . $color['lightblue'] . "  German -> " . $client['client_nickname'] . " " . $e . $set['end'] . $newline;
 						}else{
 							$ii++;
-							echo "║ > \033[32m ○ " . $client['client_nickname'] . " " . $ii . "\033[0m\n";
+							echo $set['first'] . $color['lightcyan'] . "  Intern. -> " . $client['client_nickname'] . " " . $ii . $set['end'] . $newline;
 						}
-					}catch(Exception $e){
-					    print_r("[ERROR] " . $e->getMessage() . $newline);
-					    continue;
+					} catch(Exception $e) {
+						print_r($color['red'] . "[ERROR] " . $e->getMessage() . $set['end'] . $newline);
+						continue;
 					}
-				}else{
-					echo "║ > \033[32m Real -> " . $client['client_nickname'] . "\033[0m\n";
+				} else {
+					echo "║ > " . $color['grey'] . " Real -> " . $client['client_nickname'] . $set['end'] . $newline;
 				}
 				
 			}
 		}
-		echo "╚══════════════════════════════════════════════════\n";
-		$countoftotalweb = $e + $ii;
-		$useronline = $ts3_VirtualServer->getProperty("virtualserver_clientsonline") - $ts3_VirtualServer->getProperty("virtualserver_queryclientsonline");
-		$useronlinemweb = $useronline - $countoftotalweb;
-		echo ">\033[36m There are " . $e . " Weblist User's Online from Germany!\033[0m\n";
-		echo ">\033[36m There are " . $ii . " Weblist User's Online from the no country Location!\033[0m\n";
-		echo ">\033[36m The total Weblist User count is " . $countoftotalweb . "!\033[0m\n";
-		echo ">\033[36m The total not Weblist User count is " . $useronlinemweb . "!\033[0m\n";
-		echo ">\033[36m " . $useronline . " Total User Online!\033[0m\n";
-		echo "\033[33m Checking again in " . $sleepcount/60 . " minutes...\033[0m\n";
-		$e = 0;
-		$ii = 0;
-		$prompt = "Type \033[35mend\033[0m for close this session: ";
-		echo $prompt;
-		$answer = $line->readline();
-		if($answer == "end"){
-			break;
-		}
-	}catch(Exception $e){
-	   print_r("\033[31m[ERROR] " . $e->getMessage() . $newline . "\033[0m\n");
-	   continue;
+		echo $header[8] . $newline;
+	} catch(Exception $e) {
+		print_r($color['red'] . "[ERROR] " . $e->getMessage() . $set['end'] . $newline);
 	}
-	sleep($sleepcount);
+	CheckResults();
 }
-echo ">\033[31m Turning off! Bye bye...\033[0m\n";
-try{
-	$ts3_VirtualServer->logout();
-}catch(Exception $e){
-	echo ">\033[31m Some errors to disconnect from the TeamSpeak 3 Server!\033[0m\n";
-	echo "===========================\n";
-	echo "ERROR INFOLINE\n";
-	print_r("\033[33m" . $e . "\033[0m\n");
-	echo "===========================\n";
+
+function CheckResults() {
+	global $ts3_VirtualServer;
+	global $e;
+	global $ii;
+	global $color;
+	global $newline;
+	global $set;
+	$countoftotalweb = $e + $ii;
+	$useronline = $ts3_VirtualServer->getProperty("virtualserver_clientsonline") - $ts3_VirtualServer->getProperty("virtualserver_queryclientsonline");
+	$useronlinemweb = $useronline - $countoftotalweb;
+	echo ">" . $color['cyan'] . " There are " . $e . " Weblist User's Online from Germany!" . $set['end'] . $newline;
+	echo ">" . $color['cyan'] . " There are " . $ii . " Weblist User's Online from the no country Location!" . $set['end'] . $newline;
+	echo ">" . $color['cyan'] . " The total Weblist User count is " . $countoftotalweb . "!" . $set['end'] . $newline;
+	echo ">" . $color['cyan'] . " The total not Weblist User count is " . $useronlinemweb . "!" . $set['end'] . $newline;
+	echo ">" . $color['cyan'] . " " . $useronline . " Total User Online!" . $set['end'] . $newline;
+	CloseScript();
 }
+function CloseScript() {
+	global $ts3_VirtualServer;
+	global $footerLine;
+	try {
+		$ts3_VirtualServer->logout();
+	} catch(Exception $e) {
+		echo $footerLine . $newline;
+		echo "> " . $color['red'] . "Some errors to disconnect from the TeamSpeak 3 Server!" . $set['end'] . $newline;
+		echo "> ERROR LOG: " . $color['red'] . $e . $set['end'] . $newline;
+		echo $footerLine . $newline;
+	}
+}
+
+runChecker();
+
 ?>
